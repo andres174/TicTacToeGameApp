58757d5a3b3e7536143178f32f04426986ba1ba3
andres174 <andresfer.zambrano@espam.edu.ec>
2022-11-16 22:10:53 -0500

    vista AddPlayers

--- app/src/main/java/com/andresz174/tictactoegameapp/AddPlayers.java
+++ app/src/main/java/com/andresz174/tictactoegameapp/AddPlayers.java
@@ -3,6 +3,7 @@ package com.andresz174.tictactoegameapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class AddPlayers extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class AddPlayers extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_players);
+
+        final EditText jugadorUno = findViewById(R.id.jugadorUnoNombre);
+        final EditText jugadorDos = findViewById((R.id.jugadorDosNombre));
     }
 }
