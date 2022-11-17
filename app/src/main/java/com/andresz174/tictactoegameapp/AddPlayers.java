2d4f2aade646d91aa9171a3ee584e1f7563eef97
andres174 <andresfer.zambrano@espam.edu.ec>
2022-11-17 16:32:12 -0500

    AddPlayersActivity

--- app/src/main/java/com/andresz174/tictactoegameapp/AddPlayers.java
+++ app/src/main/java/com/andresz174/tictactoegameapp/AddPlayers.java
@@ -2,8 +2,12 @@ package com.andresz174.tictactoegameapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddPlayers extends AppCompatActivity {
 
@@ -13,8 +17,27 @@ public class AddPlayers extends AppCompatActivity {
         setContentView(R.layout.activity_add_players);
 
         final EditText jugadorUno = findViewById(R.id.jugadorUnoNombre);
-        final EditText jugadorDos = findViewById((R.id.jugadorDosNombre));
+        final EditText jugadorDos = findViewById(R.id.jugadorDosNombre);
+        final Button startGameBtn = findViewById(R.id.startGameBtn);
 
-        //10:13
+        startGameBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String getJugadorUnoNombre = jugadorUno.getText().toString();
+                final String getJugadorDosNombre = jugadorDos.getText().toString();
+
+                if (getJugadorUnoNombre.isEmpty() || getJugadorDosNombre.isEmpty()){
+                    Toast.makeText(AddPlayers.this, "Ingrese los nombres de los jugadores.", Toast.LENGTH_SHORT).show();
+                }else {
+                    Intent intent = new Intent(AddPlayers.this, MainActivity.class);
+                    intent.putExtra("jugadorUno", getJugadorUnoNombre);
+                    intent.putExtra("jugadorDos", getJugadorDosNombre);
+                    startActivity(intent);
+
+                }
+            }
+        });
+
+        //10:50
     }
 }
