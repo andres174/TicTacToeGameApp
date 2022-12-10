73d92dc2b3919d1aacc9ef65e303d1ad8ea6ce8b
andres174 <zambrasda17@gmail.com>
2022-12-09 20:32:05 -0500

    onclick image

--- app/src/main/java/com/andresz174/tictactoegameapp/MainActivity.java
+++ app/src/main/java/com/andresz174/tictactoegameapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.andresz174.tictactoegameapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -20,32 +21,160 @@ public class MainActivity extends AppCompatActivity {
 
     private int totalSelectedBoxes = 1;
 
+    private LinearLayout jugadorUnoLayout, jugadorDosLayout;
+    private TextView jugadorUnoNombre, jugadorDosNombre;
+
+    private  ImageView image1,image2,image3,image4,image5,image6,image7,image8,image9;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView jugadorUnoNombre = findViewById(R.id.jugadorUnoNombre);
-        final TextView jugadorDosNombre = findViewById(R.id.jugadorDosNombre);
+        jugadorUnoNombre = findViewById(R.id.jugadorUnoNombre);
+        jugadorDosNombre = findViewById(R.id.jugadorDosNombre);
 
-        final LinearLayout jugadorUnoLayout = findViewById(R.id.jugadorUnoLayout);
-        final LinearLayout jugadorDosLayout = findViewById(R.id.jugadorDosLayout);
+        jugadorUnoLayout = findViewById(R.id.jugadorUnoLayout);
+        jugadorDosLayout = findViewById(R.id.jugadorDosLayout);
 
-        final ImageView image1 = findViewById(R.id.image1);
-        final ImageView image2 = findViewById(R.id.image2);
-        final ImageView image3 = findViewById(R.id.image3);
-        final ImageView image4 = findViewById(R.id.image4);
-        final ImageView image5 = findViewById(R.id.image5);
-        final ImageView image6 = findViewById(R.id.image6);
-        final ImageView image7 = findViewById(R.id.image7);
-        final ImageView image8 = findViewById(R.id.image8);
-        final ImageView image9 = findViewById(R.id.image9);
+        image1 = findViewById(R.id.image1);
+        image2 = findViewById(R.id.image2);
+        image3 = findViewById(R.id.image3);
+        image4 = findViewById(R.id.image4);
+        image5 = findViewById(R.id.image5);
+        image6 = findViewById(R.id.image6);
+        image7 = findViewById(R.id.image7);
+        image8 = findViewById(R.id.image8);
+        image9 = findViewById(R.id.image9);
 
-        //min27
+
+        combinationList.add(new int[]{0, 1, 2});
+        combinationList.add(new int[]{3, 4, 5});
+        combinationList.add(new int[]{6, 7, 8});
+        combinationList.add(new int[]{0, 3, 6});
+        combinationList.add(new int[]{1, 4, 7});
+        combinationList.add(new int[]{2, 5, 8});
+        combinationList.add(new int[]{2, 4, 6});
+        combinationList.add(new int[]{0, 4, 8});
+
+        final String getJugadorUnoNombre = getIntent().getStringExtra("jugadorUno");
+        final String getJugadorDosNombre = getIntent().getStringExtra("jugadorDos");
+
+        jugadorUnoNombre.setText(getJugadorUnoNombre);
+        jugadorDosNombre.setText(getJugadorDosNombre);
+
+        image1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isBoxSelectable(0)){
+
+                }
+            }
+        });
+
+        image2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isBoxSelectable(1)){
+
+                }
+            }
+        });
+
+        image3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isBoxSelectable(2)){
+
+                }
+            }
+        });
+
+        image4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isBoxSelectable(3)){
+
+                }
+            }
+        });
+
+        image5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isBoxSelectable(4)){
+
+                }
+            }
+        });
+
+        image6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isBoxSelectable(5)){
+
+                }
+            }
+        });
+
+        image7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isBoxSelectable(6)){
+
+                }
+            }
+        });
+
+        image8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isBoxSelectable(7)){
+
+                }
+            }
+        });
+
+        image9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (isBoxSelectable(8)){
+
+                }
+            }
+        });
+
+        /*jugadorUnoLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        jugadorDosLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });*/
 
 
 
 
 
     }
+
+    private void performAction(){
+        //33:55
+    }
+
+    private boolean isBoxSelectable(int boxPosition){
+        boolean response = false;
+        if (boxPositions[boxPosition] == 0){
+            response = true;
+        }
+        return response;
+    }
+
+
 }
